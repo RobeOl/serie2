76a901afee498f0540290b887563d68811d18384
robolmi <r.olmi@ifac.cnr.it>
2026-03-27 13:56:55 +0100

    aggiornamento

--- quaternary.py
+++ quaternary.py
@@ -1,6 +1,7 @@
 import random
 from music21 import *
 import copy
+import time
 
 def f_octave(x):
     # limit notes to octaves 4-5
@@ -11,79 +12,275 @@ def f_durata(x):
 	# x.duration.quarterLength = random.choice([1, 1/2, 1/4])
 	x.duration.quarterLength = random.choice([1, 1/2, 1/4])
 
-def genera_quaternary(i,j,ii,jj):
+def genera_quaternary(tipo,note_len,i,j,ii,jj,starting_note=0):
     #lista_note = ["C4", "C#4", "D4", "D#4", "E4", "F4", "F#4", "G4", "G#4", "A4"]
     #nota_in= lista_note[starting_note]
     #x = note.Note(nota_in)
-    #x = note.Note(starting_note)
-    x = note.Note("C4")
+    x = note.Note(starting_note)
+    #x = note.Note("C4")
 
     notes = []
 
     # first interval, starting with C4
     note1 = x
 
+    if tipo=="sequence-constrained":
+        # sequence-constrained
 
-    # free 
-    notes.append(copy.deepcopy(note1))
-    prima = note1.name
-
-    note1.transpose(i,inPlace=True)
-    f_octave(note1)
-    f_durata(note1)
-    notes.append(copy.deepcopy(note1))
-    seconda = note1.name
-    first_couple = [prima,seconda]
-
-    # print('FIRST: ',first_couple)
-
-    # first jump
-    note1.transpose(j,inPlace=True)
-    f_octave(note1)
-    f_durata(note1)
-    notes.append(copy.deepcopy(note1))
-    prima = note1.name
-
-    # second interval
-    note1.transpose(ii,inPlace=True)
-    f_octave(note1)
-    f_durata(note1)
-    notes.append(copy.deepcopy(note1))
-    seconda = note1.name
-
-    # second jump
-    note1.transpose(jj,inPlace=True)
-    f_octave(note1)
-    f_durata(note1)
-    notes.append(copy.deepcopy(note1))
-    prima = note1.name
-
-    # begin repetition
-    # first interval
-    note1.transpose(i,inPlace=True)
-    f_octave(note1)
-    f_durata(note1)
-    notes.append(copy.deepcopy(note1))
-    seconda = note1.name
-
-    condition = True
-    # following intervals
-    while condition:
-        note1.transpose(j,inPlace=True)
-        f_octave(note1)
         f_durata(note1)
         notes.append(copy.deepcopy(note1))
         prima = note1.name
 
-        note1.transpose(ii,inPlace=True)
+        note1.transpose(i,inPlace=True)
         f_octave(note1)
-        f_durata(note1)
         notes.append(copy.deepcopy(note1))
         seconda = note1.name
-            
+        first_couple = [prima,seconda]
+
+        # print('FIRST: ',first_couple)
+
+        # first jump
+        note1.transpose(j,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # second interval
+        note1.transpose(ii,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        # second jump
         note1.transpose(jj,inPlace=True)
         f_octave(note1)
+        # change time
         f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # begin repetition
+        # first interval
+        note1.transpose(i,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        condition = True
+        # following intervals
+        while condition:
+            note1.transpose(j,inPlace=True)
+            f_octave(note1)
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(ii,inPlace=True)
+            f_octave(note1)
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+                
+            note1.transpose(jj,inPlace=True)
+            f_octave(note1)
+            # change time
+            f_durata(note1)
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(i,inPlace=True)
+            f_octave(note1)
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+            current_couple = [prima,seconda]
+            condition = (current_couple!=first_couple)
+            # print('CURRENT: ',current_couple)
+
+    elif tipo=="length-constrained":
+        # length-constrained
+        conta = 1
+        note_number = random.choice([2,3,5,6])
+
+        f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        note1.transpose(i,inPlace=True)
+        conta = conta+1
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+        first_couple = [prima,seconda]
+
+        # print('FIRST: ',first_couple)
+
+        # first jump
+        note1.transpose(j,inPlace=True)
+        f_octave(note1)
+        if conta==note_number:
+            f_durata(note1)
+            conta=1
+        else:
+            conta = conta+1
+
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # second interval
+        note1.transpose(ii,inPlace=True)
+        f_octave(note1)
+        if conta==note_number:
+            f_durata(note1)
+            conta=1
+        else:
+            conta = conta+1
+
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        # second jump
+        note1.transpose(jj,inPlace=True)
+        f_octave(note1)
+        if conta==note_number:
+            f_durata(note1)
+            conta=1
+        else:
+            conta = conta+1
+
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # begin repetition
+        # first interval
+        note1.transpose(i,inPlace=True)
+        f_octave(note1)
+        if conta==note_number:
+            f_durata(note1)
+            conta=1
+        else:
+            conta = conta+1
+
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        condition = True
+        # following intervals
+        while condition:
+            note1.transpose(j,inPlace=True)
+            f_octave(note1)
+            if conta==note_number:
+                f_durata(note1)
+                conta=1
+            else:
+                conta = conta+1
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(ii,inPlace=True)
+            f_octave(note1)
+            if conta==note_number:
+                f_durata(note1)
+                conta=1
+            else:
+                conta = conta+1
+
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+                
+            note1.transpose(jj,inPlace=True)
+            f_octave(note1)
+            if conta==note_number:
+                f_durata(note1)
+                conta=1
+            else:
+                conta = conta+1
+
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(i,inPlace=True)
+            f_octave(note1)
+            if conta==note_number:
+                f_durata(note1)
+                conta=1
+            else:
+                conta = conta+1
+                
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+            current_couple = [prima,seconda]
+            condition = (current_couple!=first_couple)
+            # print('CURRENT: ',current_couple)
+
+    elif tipo=="constant":
+        # constant 
+        x.duration.quarterLength = note_len
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        note1.transpose(i,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+        first_couple = [prima,seconda]
+
+        # print('FIRST: ',first_couple)
+
+        # first jump
+        note1.transpose(j,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # second interval
+        note1.transpose(ii,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        # second jump
+        note1.transpose(jj,inPlace=True)
+        f_octave(note1)
+        # change time
+        #f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # begin repetition
+        # first interval
+        note1.transpose(i,inPlace=True)
+        f_octave(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        condition = True
+        # following intervals
+        while condition:
+            note1.transpose(j,inPlace=True)
+            f_octave(note1)
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(ii,inPlace=True)
+            f_octave(note1)
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+                
+            note1.transpose(jj,inPlace=True)
+            f_octave(note1)
+            # change time
+            #f_durata(note1)
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(i,inPlace=True)
+            f_octave(note1)
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+            current_couple = [prima,seconda]
+            condition = (current_couple!=first_couple)
+            #print('CURRENT: ',current_couple)
+
+    else:
+        # free 
         notes.append(copy.deepcopy(note1))
         prima = note1.name
 
@@ -92,9 +289,68 @@ def genera_quaternary(i,j,ii,jj):
         f_durata(note1)
         notes.append(copy.deepcopy(note1))
         seconda = note1.name
-        current_couple = [prima,seconda]
-        condition = (current_couple!=first_couple)
-        #print('CURRENT: ',current_couple)
+        first_couple = [prima,seconda]
+
+        # print('FIRST: ',first_couple)
+
+        # first jump
+        note1.transpose(j,inPlace=True)
+        f_octave(note1)
+        f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # second interval
+        note1.transpose(ii,inPlace=True)
+        f_octave(note1)
+        f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        # second jump
+        note1.transpose(jj,inPlace=True)
+        f_octave(note1)
+        f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        prima = note1.name
+
+        # begin repetition
+        # first interval
+        note1.transpose(i,inPlace=True)
+        f_octave(note1)
+        f_durata(note1)
+        notes.append(copy.deepcopy(note1))
+        seconda = note1.name
+
+        condition = True
+        # following intervals
+        while condition:
+            note1.transpose(j,inPlace=True)
+            f_octave(note1)
+            f_durata(note1)
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(ii,inPlace=True)
+            f_octave(note1)
+            f_durata(note1)
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+                
+            note1.transpose(jj,inPlace=True)
+            f_octave(note1)
+            f_durata(note1)
+            notes.append(copy.deepcopy(note1))
+            prima = note1.name
+
+            note1.transpose(i,inPlace=True)
+            f_octave(note1)
+            f_durata(note1)
+            notes.append(copy.deepcopy(note1))
+            seconda = note1.name
+            current_couple = [prima,seconda]
+            condition = (current_couple!=first_couple)
+            #print('CURRENT: ',current_couple)
 
     
         
