862002b5e9775bfea90844dbf1a465874e9364bc
robolmi <r.olmi@ifac.cnr.it>
2026-03-27 13:37:40 +0100

    sostituito app

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ from quaternary import genera_quaternary
 
 app = Flask(__name__)
 #CORS(app)
-CORS(app, origins="https://murosigma.it")
+CORS(app, origins="https://murosigma.it/rob")
 
 def generate_music(start_note, sequence_type, tempo_type, harmony,
                    note_length=1,
