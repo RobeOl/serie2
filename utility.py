57e196434857600ef2d6e4634dc45bd3e9020e1d
RobeOl <robe.olmi@gmail.com>
2026-05-27 12:35:07 +0200

    Add files via upload

--- utility.py
+++ utility.py
@@ -2,16 +2,14 @@ import random
 from music21 import *
 
 def f_octave(x, ottave, oct):
-    oct_max = oct + ottave  # ottava massima consentita
+    midi_start = (oct + 1) * 12       # MIDI del Do dell'ottava di partenza (C4=60 se oct=4)
+    midi_min   = midi_start - 12      # sempre 1 ottava sotto il Do di partenza
+    midi_max   = midi_start + ottave * 12  # ottave=1 → C5, ottave=2 → C6
 
-    # Correggi se troppo alta
-    while x.octave > oct_max:
-        x.octave -= 1
-
-    # Correggi se troppo bassa
-    while x.octave < oct:
-        x.octave += 1
+    while x.pitch.midi < midi_min:
+        x.pitch.midi += 12
+    while x.pitch.midi > midi_max:
+        x.pitch.midi -= 12
 
 def f_durata(x):
     x.duration.quarterLength = random.choice([1, 1/2, 1/4])
-
